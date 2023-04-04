3cf13307658287f38603dcf7b92b67d9c66c4ccb
MEGAVARTHINI M <109512565+Megavarthini@users.noreply.github.com>
2023-04-04 20:06:51 +0530

    Add files via upload

---
+++ languages.js
@@ -0,0 +1,41 @@
+const languages = [
+    {
+        no: "16",
+        name: "English",
+        native: "English",
+        code: "en",
+    },
+    {
+        no: "20",
+        name: "French",
+        native: "Français",
+        code: "fr",
+    },
+
+    {
+        no: "23",
+        name: "German",
+        native: "Deutsch",
+        code: "de",
+    },
+
+    {
+        no: "27",
+        name: "Hindi",
+        native: "हिन्दी",
+        code: "hi",
+    },
+
+    {
+        no: "33",
+        name: "Japanese",
+        native: "日本語",
+        code: "ja",
+    },
+
+
+
+
+
+
+];
